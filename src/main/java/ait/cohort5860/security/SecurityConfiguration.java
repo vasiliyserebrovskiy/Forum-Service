d06f23df5f00d887882a9849a3d4b754998a28af
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-07-01 14:11:44 +0200

    SecurotyConfiguration changed: added POST and PATCH authorization for post and comment

--- src/main/java/ait/cohort5860/security/SecurityConfiguration.java
+++ src/main/java/ait/cohort5860/security/SecurityConfiguration.java
@@ -33,6 +33,10 @@ public class SecurityConfiguration {
                     .access(new WebExpressionAuthorizationManager("#login == authentication.name"))
                 .requestMatchers(HttpMethod.DELETE, "/account/user/{login}")
                     .access(new WebExpressionAuthorizationManager("#login == authentication.name or hasRole('ADMINISTRATOR')"))
+                .requestMatchers(HttpMethod.POST, "/forum/post/{author}")
+                    .access(new WebExpressionAuthorizationManager("#author == authentication.name"))
+                .requestMatchers(HttpMethod.PATCH, "/forum/post/{id}/comment/{author}")
+                .access(new WebExpressionAuthorizationManager("#author == authentication.name"))
                 .anyRequest()
                     .authenticated());
         return http.build();
