ce0cdec02bdeae305d9c9bb0790b07b186845337
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-07-04 09:31:04 +0200

    Uploading and downloading files to post init

---
+++ src/main/java/ait/cohort5860/post/dto/FileResponseDto.java
@@ -0,0 +1,10 @@
+package ait.cohort5860.post.dto;
+
+/**
+ * @author Vasilii Serebrovskii
+ * @version 1.0 (04.07.2025)
+ */
+public class FileResponseDto {
+    private String filename;
+    private String downloadUrl;
+}
